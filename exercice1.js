e17aa39c7623960f5531ad05a76d35450502240d
jordan houyon <116800565+Jordan-Houyon@users.noreply.github.com>
2022-11-22 14:23:37 +0100

    exo 71 fini

--- exercice1.js
+++ exercice1.js
@@ -4,14 +4,15 @@ document.getElementById('viewport').appendChild(monDiv);
 
 let secondDiv= document.createElement('div');
 document.getElementById('viewport').appendChild(secondDiv);
-
+secondDiv.innerHTML="hello";
 monDiv.innerHTML="Ce div a été crée en utilisant Javascript";
 
 secondDiv.style.backgroundColor="red";
 secondDiv.style.width="100px";
-secondDiv.stylr.hright="100px";
+secondDiv.style.hright="100px";
 
-document.getElementById("aSupprimer");
+let aSupprimer = document.getElementById(`aSupprimer`)
+aSupprimer.remove();
 
 
 

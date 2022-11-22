3e71c74a0cbcd575a4c444a04fdb44d99920fd1f
jordan houyon <116800565+Jordan-Houyon@users.noreply.github.com>
2022-11-22 10:21:07 +0100

    exo 71 fini

--- exercice1.js
+++ exercice1.js
@@ -1,7 +1,17 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
 
 document.getElementById('viewport').appendChild(monDiv);
 
+let secondDiv= document.createElement('div');
+document.getElementById('viewport').appendChild(secondDiv);
+
+monDiv.innerHTML="Ce div a été crée en utilisant Javascript";
+
+secondDiv.style.backgroundColor="red";
+secondDiv.style.width="100px";
+secondDiv.stylr.hright="100px";
+
+document.getElementById("aSupprimer");
 
 
 
